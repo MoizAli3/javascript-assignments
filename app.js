b30d22576f4e1ec748f885d14378d3e2fb543b68
Moiz Ali <moizk5590@gmail.com>
2024-09-01 08:37:32 +0500

    .

--- app.js
+++ app.js
@@ -53,3 +53,72 @@ alert("I am trying to learn from the book " + book);
 
 // Question No # 08
 document.write("Yah! I can write HTML Content through Javascript");
+
+// ****Asssignment No # 03*****
+// ******** Variables For Numbers *********
+
+// Question No # 01
+var age = 15;
+alert("I am" + age + "years old");
+
+// Question No # 02
+var vistedNumers = 0;
+alert("You have Visited" + vistedNumers++ + "times");
+
+// Question No # 03
+var birth = 1999;
+document.write("\n My Birth year is" + birth);
+document.write("\nData Type of my decleared variable is " + typeof birth);
+
+// Question No # 04
+var visitorName = "Moiz Ali";
+var productTitle = "T-shirts";
+var quantity = 5;
+document.write(
+  visitorName + "Ordered" + quantity + productTitle + "on GulAhmed Store"
+);
+
+// ****Asssignment No # 04*****
+// ******** Legal or Illegal *********
+
+// Question No # 01
+var age, place, things;
+
+// Question No # 02
+var age, place, things, name, animal;
+// var .age, 2place, \things, na me, an""imal;
+
+// Question No # 03
+document.write("<h1>Rules For naming JS variables</h2>\n\n");
+document.write(
+  "Variable names can only contain, numbers, $ and _ For example: $my_1stVariable"
+);
+document.write(
+  "Variable must begin with a letter, S or _ . For example: Sname, _name or name"
+);
+
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS keywords");
+
+// ****Asssignment No # 05*****
+// ******** Math Operations *********
+
+// Question No # 01
+var num1 = 2;
+var num2 = 2;
+var sum = num1 + num2;
+document.write("sum of" + num1 + "and" + num2 + "is" + sum);
+
+// Question No # 02
+var subtraction = num1 - num2;
+document.write("subtraction of" + num1 + "and" + num2 + "is" + subtraction);
+var multiplicaiton = num1 * num2;
+document.write(
+  "multiplicaiton of" + num1 + "and" + num2 + "is" + multiplicaiton
+);
+var division = num1 / num2;
+document.write("division of" + num1 + "and" + num2 + "is" + division);
+var remainder = num1 % num2;
+document.write("Remainder" + num1 + "and" + num2 + "is" + remainder);
+
+// Question No # 03
