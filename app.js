3e4cbbeb6b1b91a610f4824571eb4f607b883394
Moiz Ali <moizk5590@gmail.com>
2024-09-01 08:03:30 +0500

    Assignment No 1 & 2

---
+++ app.js
@@ -0,0 +1,55 @@
+// **** Asssignment No # 01 *****
+// ********** Alerts ************
+
+// Question No # 01
+alert("Welcome User ! Good Morning");
+
+// Question No # 02
+alert("Error! Please a Enter a valid Password");
+
+// Question No # 03
+alert("Welcome to js Land \nHappy Coding! ");
+
+// Question No # 04
+alert("Welcome to js Land");
+alert("Happy Coding! ");
+
+// Question No # 05
+console.log(alert("Hello I can rus JS through my web browser's console"));
+
+// ****Asssignment No # 02*****
+// ******** Variables *********
+
+// Question No # 01
+var username;
+
+// Question No # 02
+var myName = "Moiz Ali";
+
+// Question No # 03
+var message;
+message = "Moiz Ali";
+alert(message);
+
+// Question No # 04
+var name = "Moiz Ali";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(course);
+
+// Question No # 05
+var trickyName = "PIZZA \nPIZZ\nPIZ\nPIZ\nPI\nP";
+alert(trickyName);
+
+// Question No # 06
+var email = "moizali5590@gmail.com";
+alert("My email address is " + email);
+
+// Question No # 07
+var book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the book " + book);
+
+// Question No # 08
+document.write("Yah! I can write HTML Content through Javascript");
